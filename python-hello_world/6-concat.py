383202bcbc7e92db8ff4b37f8241504f57feeac9
tonywahome <a.wambugu@alustudent.com>
2024-02-27 16:52:37 +0200

    concat

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-<<<<<<< HEAD
 strn = str1+" "+str2
 print(f"Welcome to {strn}!")
-=======
-print('str1' 'str2')
-print(f"Welcome to {str1} {str2}!")
->>>>>>> 6a180c7340c495f1d03c2a7d8757ee181ab0254d
