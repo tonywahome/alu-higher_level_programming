066d35d89f59d26fb8eae731c973235c2d9a02b0
tonywahome <a.wambugu@alustudent.com>
2024-02-27 07:33:55 -0800

    8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,3 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(f"str[39:66]+' '+'with Python'")
-
