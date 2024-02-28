50c0618e11b35b9bb2e8af509378471dbc2caaa8
tonywahome <a.wambugu@alustudent.com>
2024-02-28 11:58:25 -0800

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for p in range(10):
-    for m in range(p + 1, 10):
-        print("{:p}{:p}".format(p, m), end=", " if d < 8 else "\n")
+for d in range(10):
+    for a in range(d + 1, 10):
+        print("{:d}{:d}".format(d, a), end=", " if d < 8 else "\n")
