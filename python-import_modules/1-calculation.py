72118cae21a3fdcf6253e4529ddfe1f5111938e1
tonywahome <a.wambugu@alustudent.com>
2024-02-29 05:18:49 -0800

    1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
-
 a = 10
 b = 5
-
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, multiply, divide
 sum_result = (a, b)
 difference_result = subtract(a, b)
 product_result = multiply(a, b)
