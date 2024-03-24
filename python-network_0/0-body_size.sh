3abaf78a81f237b1308a6078173aa79e588f9dd0
tonywahome <a.wambugu@alustudent.com>
2024-03-24 15:20:26 +0200

    12

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# Get the byte size of the HTTP response header for a given URL
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
