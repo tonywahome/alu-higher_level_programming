e8cc772330551c9ac9165c3ccfb8f8e315ad2e10
tonywahome <a.wambugu@alustudent.com>
2024-06-07 23:40:24 +0200

    javascript tasks

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const argv = process.argv.slice(2);
-const message = argv.length > 0 ? (argv.length === 1 ? 'Argument found' : 'Arguments found') : 'No argument';
-console.log(message);
+const arg = process.argv[2];
+if (arg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
